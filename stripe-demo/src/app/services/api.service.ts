8dbc7a4fe1e0a697388bfcff4b632fb68c9d39a2
jamalmoosvi <jamalmoosvi@gmail.com>
2020-09-30 22:15:39 +0500

    updated apidomain

--- stripe-demo/src/app/services/api.service.ts
+++ stripe-demo/src/app/services/api.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class ApiService {
 
   constructor(private http: HttpClient) { }
-  apiDomain = 'https://serene-citadel-01484.herokuapp.com/';
+  apiDomain = 'https://serene-citadel-01484.herokuapp.com';
   //apiDomain = 'http://localhost:8080';
 
   helloWorld() {
